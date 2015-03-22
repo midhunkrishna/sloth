962b568095d389d25a58c63f352d916271952874
midhun krishna <kr1shn4.m1dhun@gmail.com>
2015-03-22 23:19:29 +0530

    [].substring search: searches for all the elements in an array that has a given substring

--- lib/sloth/array_helpers.rb
+++ lib/sloth/array_helpers.rb
@@ -3,5 +3,14 @@ module Sloth
     def cleanup
       flatten.compact.uniq.reject(&:empty?)
     end
+
+    def substring_search string_pattern
+      pattern = Regexp.new(string_pattern.to_s)
+      result = []
+      self.each do |element|
+        result << element if element =~ pattern
+      end
+      result
+    end
   end
 end
