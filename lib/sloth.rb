5d0be9f3064c87f5251da54f125adf0e2b2be0dd
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-03-24 22:00:30 +0530

    Add StringHelper: Validate email strings

--- lib/sloth.rb
+++ lib/sloth.rb
@@ -1,6 +1,7 @@
 require "sloth/version"
 require "sloth/array_helpers"
 require "sloth/hash_helpers"
+require "sloth/string_helpers"
 
 module Sloth
 end
@@ -11,4 +12,8 @@ end
 
 class Hash
   include Sloth::HashHelpers
+end
+
+class String
+  include Sloth::StringHelpers
 end
